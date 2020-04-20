9c9ba84af908ca8b93ac2e4432b576cac83bcbba
1Edwar <60082262+1Edwar@users.noreply.github.com>
2020-04-20 10:48:37 -0700

    Add files via upload

---
+++ 591_bitcoin_project.py
@@ -0,0 +1,17 @@
+import networkx as nx
+import pandas as pd
+import cpalgorithm as cp
+
+df = pd.read_csv('bitcoin_notime.csv', sep = ',')     #load data from dataset
+
+G = nx.from_pandas_edgelist(df)
+
+algorithm = cp.KM_config()
+algorithm.detect(G)
+c = algorithm.get_pair_id()
+x = algorithm.get_coreness()
+
+print(c)
+# print('Name\tPairID\tCoreness')
+# for key, value in sorted(c.items(), key=lambda x: x[1]):
+#     print('%s\t%d\t%f' %(key, c[key], x[key]))
