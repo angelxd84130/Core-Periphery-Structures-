2173da5b56b12475c0573d50015fd385eef7c1de
ANGEL <angelxd84130@gmail.com>
2020-04-20 18:52:29 -0700

    Update 591_bitcoin_project.py

--- 591_bitcoin_project.py
+++ 591_bitcoin_project.py
@@ -2,7 +2,7 @@ import networkx as nx
 import pandas as pd
 import cpalgorithm as cp
 
-df = pd.read_csv('bitcoin_notime2.csv', sep = ',')     #load data from dataset
+df = pd.read_csv('Using dataset.csv', sep = ',')     #load data from dataset
 
 G = nx.from_pandas_edgelist(df)
 
