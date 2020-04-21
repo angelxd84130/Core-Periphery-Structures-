e8c71dcad83f3390d29bfe6010fefb55d35d53c9
1Edwar <60082262+1Edwar@users.noreply.github.com>
2020-04-20 17:15:49 -0700

    Update 591_bitcoin_project.py

--- 591_bitcoin_project.py
+++ 591_bitcoin_project.py
@@ -2,7 +2,7 @@ import networkx as nx
 import pandas as pd
 import cpalgorithm as cp
 
-df = pd.read_csv('bitcoin_notime.csv', sep = ',')     #load data from dataset
+df = pd.read_csv('bitcoin_notime2.csv', sep = ',')     #load data from dataset
 
 G = nx.from_pandas_edgelist(df)
 
@@ -11,7 +11,6 @@ algorithm.detect(G)
 c = algorithm.get_pair_id()
 x = algorithm.get_coreness()
 
-print(c)
-# print('Name\tPairID\tCoreness')
-# for key, value in sorted(c.items(), key=lambda x: x[1]):
-#     print('%s\t%d\t%f' %(key, c[key], x[key]))
+print('Name\tPairID\tCoreness')
+for key, value in sorted(c.items(), key=lambda x: x[1]):
+    print('%s\t%d\t%f' %(key, c[key], x[key]))
